cea01f0edb077d4d22377d903bb586bc4a3abbb9
João Vitor <olhierturcojoaovitor@gmail.com>
2026-06-19 13:18:28 -0300

    att

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,27 @@
 let rotacaoAtual = 0
-function girar(){
+
+const girar = () => {
     const roleta = document.getElementById("roleta");
     
     const giro = Math.floor(Math.random() * 360) + 4600;
     rotacaoAtual += giro;
     roleta.style.transform = `rotate(${rotacaoAtual}deg)`;
+}
+
+window.addEventListener('keydown', (a) => {
+    if (a.key === "Enter") {
+        girar()
+    }
+})
+
+const trocarTema = () => {
+    const body = document.body;
+    const btn_dark = document.getElementById("btn-dark");
+    const btn_light = document.getElementById("btn-light")
+
+    body.classList.toggle("dark");
+    btn_dark.classList.toggle("display-none");
+    btn_light.removeAttribute("display-none")
 }
 
 
